5d610a30e972f1660e2138c542f2df46ead1add3
Eujine <sponsor85@mail.ru>
2019-02-05 00:11:38 +0200

    init commit

---
+++ CustomConfig/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomConfig
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			//https://msdn.microsoft.com/en-us/library/2tw134k3.aspx
+
+            setConfigFileAtRuntime(@".\CustomConfig\CustomConfig\bin\Debug\1.config");
+            Console.WriteLine(ConfigurationManager.AppSettings["test"].ToString());
+            StartupFoldersConfigSection section = (StartupFoldersConfigSection)ConfigurationManager.GetSection("StartupFoldersGroup/StartupFolders");
+
+            if (section != null)
+            {
+                Console.WriteLine(section.FolderItems[0].folderType);
+                Console.WriteLine(section.FolderItems[0].Path);
+            }
+        }
+        protected static void setConfigFileAtRuntime(string args)
+        {   // change config file name 
+            //https://www.codeproject.com/Articles/616065/Why-Where-and-How-of-NET-Configuration-Files
+            AppDomain.CurrentDomain.SetData("APP_CONFIG_FILE", args);
+        }
+    }
+}
---
+++ CustomConfig/StartupFoldersConfigSection.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomConfig
+{// https://habrahabr.ru/post/128517/
+ // https://msdn.microsoft.com/en-us/library/system.appdomainsetup.configurationfile.aspx
+    public class StartupFoldersConfigSection: ConfigurationSection
+    {
+        [ConfigurationProperty("Folders")]
+        public FoldersCollection FolderItems
+        {
+            get { return (FoldersCollection)(base["Folders"]); }
+        }
+    }
+    [ConfigurationCollection( typeof(FolderElement), AddItemName="Folder")]
+    public class FoldersCollection : ConfigurationElementCollection
+    {
+        protected override ConfigurationElement CreateNewElement()
+        {
+            return new FolderElement();
+        }
+        protected override object GetElementKey(ConfigurationElement element)
+        {
+            return ((FolderElement)(element)).folderType;
+        }
+        public FolderElement this[int idx]
+        {
+            get { return (FolderElement)BaseGet(idx); }
+        }
+    }
+    public class FolderElement : ConfigurationElement
+    {
+        [ConfigurationProperty("folderType", DefaultValue = "", IsKey = false, IsRequired = false)]
+        public string folderType
+        {
+            get { return ((string)(base["folderType"])); }
+            set { base["folderType"] = value; }
+        }
+
+        [ConfigurationProperty("path", DefaultValue = "", IsKey = false, IsRequired = false)]
+        public string Path
+        {
+            get { return ((string)(base["path"]));}
+            set { base["path"] = value; }
+        }
+    }
+}
